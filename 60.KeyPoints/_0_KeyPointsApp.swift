f444e59d8b6881571e1fc9d9feff0765bcfb85a1
Valentin <pamerra@mail.ru>
2025-07-28 17:30:50 +0300

    Initial Commit

---
+++ 60.KeyPoints/_0_KeyPointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_KeyPointsApp.swift
+//  60.KeyPoints
+//
+//  Created by Валентин on 28.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct _0_KeyPointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
